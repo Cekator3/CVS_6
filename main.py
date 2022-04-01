acf1a7fdb4538091121a1de7f9afffd237a7dc4e
Yaroslav_Kuznetsov <yaroslav_kuznetsov_3002@mail.ru>
2022-04-01 11:35:13 +0500

    second

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def func1(strs, s):
     else:
         strs.insert(len(strs)/2, s)
     return strs
+
+def func2(strs,s,str):
+    if (s<0 or len(strs)<s):
+        print("operation is not possible")
+    else:
+        strs.insert(s, str)
+    return
